0c85f9c1ea17e59cdaf8362de67862be5b7504f5
Gratianestha <98179730+Gratianestha@users.noreply.github.com>
2022-11-22 12:47:26 +0700

    tesssss

---
+++ app/src/main/java/edu/bluejack22_1/pillreminder/RegisterActivity.java
@@ -0,0 +1,14 @@
+package edu.bluejack22_1.pillreminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
