134a7a3c350816f907dbbe0f14e9b4ffe1d82074
jeremyloa <79776290+jeremyloa@users.noreply.github.com>
2022-11-25 13:18:01 +0700

    logout and splash

--- app/src/main/java/edu/bluejack22_1/pillreminder/MainActivity.kt
+++ app/src/main/java/edu/bluejack22_1/pillreminder/MainActivity.kt
@@ -1,12 +1,25 @@
 package edu.bluejack22_1.pillreminder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import edu.bluejack22_1.pillreminder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        //testing
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        logoutListener()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+    }
+
+    private fun logoutListener(){
+        binding.toLogoutBtn.setOnClickListener {
+            UserCurrent.logout()
+            startActivity(Intent(this, Splash::class.java))
+            finish()
+        }
     }
 }
