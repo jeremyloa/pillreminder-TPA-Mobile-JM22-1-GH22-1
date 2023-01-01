d8bda1a1907fd7ba25094b81e4fa28bda047eeb6
Gratianestha <98179730+Gratianestha@users.noreply.github.com>
2023-01-01 08:35:46 +0700

    notif1

--- app/src/main/java/edu/bluejack22_1/pillreminder/controller/treatment/TreatmentsMain.kt
+++ app/src/main/java/edu/bluejack22_1/pillreminder/controller/treatment/TreatmentsMain.kt
@@ -1,30 +1,17 @@
 package edu.bluejack22_1.pillreminder.controller.treatment
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.viewpager2.widget.ViewPager2
-import edu.bluejack22_1.pillreminder.R
-import edu.bluejack22_1.pillreminder.adapter.ActivityAdapter
+import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 
 class TreatmentsMain : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_treatments_main, container, false)
-    }
 
 
 
